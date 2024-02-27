0084e737d0ce76ca0cac47ea00a71790e5e2d98e
roberto.nasc93 <roberto.nasc93@gmail.com>
2024-02-27 21:48:59 +0000

    useForm hook done, + pregress bar n form steps logic done

--- src/hooks/useForm.tsx
+++ src/hooks/useForm.tsx
@@ -3,8 +3,18 @@ import { useState } from "react";
 export function useForm(steps: any) {
   const [currentStep, setCurrentStep] = useState(0);
   const [currentComponent, setCurrentComponent] = useState(steps[currentStep]);
+
+  function changeStep(i: number, e: any) {
+    e.preventDefault()
+
+    if(i < 0 || i> steps.length) return 
+
+    setCurrentStep(i);
+  }
   return {
     currentStep,
     currentComponent: steps[currentStep],
+    changeStep,
+    isLastStep: currentStep + 1 === steps.length ? true : false,
   };
 }
--- src/pages/reserve_form/index.tsx
+++ src/pages/reserve_form/index.tsx
@@ -1,4 +1,6 @@
-import { Header } from "../../components/ui/Header";
+import { mdiArrowLeftCircleOutline } from "@mdi/js";
+import Icon from "@mdi/react";
+import { Button } from "../../components/button/Button";
 import { useForm } from "../../hooks/useForm";
 import StepFive from "./StepFive";
 import StepFour from "./StepFour";
@@ -19,22 +21,57 @@ export const ReserveForm = () => {
     <StepSeven />,
   ];
 
-  const {currentStep, currentComponent} = useForm(formComponents)
+  const { currentStep, currentComponent, changeStep, isLastStep } = useForm(formComponents);
+  const progressPercent = (currentStep / (formComponents.length - 1)) * 100;
+
   return (
     <div>
-      <Header>
+      <header className="shadow z-50 relative bg-white rounded-b-3xl mb-10 w-full items-center justify-between py-6 flex px-10  ">
+        <span onClick={(e) => changeStep(currentStep - 1, e)}>
+          <Icon
+            path={mdiArrowLeftCircleOutline}
+            className="text-[#ccc] font-light cursor-pointer"
+            title="Go back"
+            size={2}
+          />
+        </span>
         <div>
           <h1 className="text-primary text-end text-base font-bold">
             Reserva campos de feerias 20224
           </h1>
           <p>Semanas e protocolos</p>
+          <div className="flex items-center space-x-2">
+            <span className="text-sm text-black"></span>
+            <div className="flex bg-secondary justify-between items-center pl-[6px] pr-3 h-4 bg-gray-200 rounded-[100px] w-full">
+              <div className="bg-[#544C94] w-full h-[6px] mr-4 rounded-full">
+                <div
+                  className="h-[6px] bg-[#61B232] flex justify-end items-center rounded-full"
+                  style={{ width: `${progressPercent}%` }}
+                />
+              </div>
+
+              <span className="text-white text-[12px]">
+                {currentStep + 1}/{formComponents.length}
+              </span>
+            </div>
+          </div>
         </div>
-      </Header>
+      </header>
       <div className="bg-primary z-10 rounded-b-[14px] -top-16 relative h-[196px] w-full"></div>
 
       <main className="px-10">
-        <form  className="">
-            {currentComponent}
+        <form className="" onSubmit={(e) => changeStep(currentStep + 1, e)}>
+          {currentComponent}
+          {!isLastStep ? (
+              <Button type={"submit"} variants="primary" fullWidth>
+              Continuar
+            </Button>
+          ) : (
+            <Button type={"button"} variants="primary" fullWidth>
+            Enviar
+          </Button>
+          )}
+        
         </form>
       </main>
     </div>
