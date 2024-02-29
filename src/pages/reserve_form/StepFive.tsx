f6632128044e599d4e878cbde28785feb797b97b
roberto.nasc93 <roberto.nasc93@gmail.com>
2024-02-29 17:59:07 +0000

    Step5 done styling

--- src/pages/reserve_form/StepFive.tsx
+++ src/pages/reserve_form/StepFive.tsx
@@ -3,6 +3,7 @@ import { DropDown } from "../../components/ui/DropDown";
 import { RadioImage } from "../../components/ui/RadioImage";
 import Cama from "./assets/camaratas.jpeg";
 import Glaping from "./assets/glamping.jpeg";
+import { Radiooption } from "../../components/ui/RadioOption";
 
 const StepFive = () => {
   const options = [
@@ -52,6 +53,81 @@ const StepFive = () => {
       </div>
       <hr className="border-neutral border-4 my-6" />
       <h2 className="uppercase font-bold mb-4 ">Aluguer kit de roupa cama</h2>
+      <div className="my-4 bg-[#463D8C1A] rounded-[10px] p-4 flex-col ">
+        <h3 className="font-semibold text-left text-base mb-2">
+          Semana 1 a 6 Julho
+        </h3>
+        <div className="flex items-center justify-center w-full">
+          <Radiooption
+            options={[
+              { label: "Sim", value: "option1" },
+              { label: "Não", value: "option2" },
+            ]}
+            selectedValue={selectedValue}
+            onChange={handleRadioChange}
+          />
+        </div>
+      </div>
+      <hr className="border-primary border-4 my-8" />
+      <h2 className="text-lg mb-6">
+        <span className="font-semibold text-primary text-2xl mr-3">5.3</span>A
+        Durante o tempo que passar connosco haverá sempre alguém a registar a experiência. Mas claro que só com a devida autorização. 
+      </h2>
+      <h3 className="font-bold uppercase mb-4">cedência de imagem</h3>
+      <div className="my-4 bg-[#463D8C1A] rounded-[10px] p-4 flex-col ">
+        <h3 className="font-semibold text-left text-base mb-2">
+          Semana 1 a 6 Julho
+        </h3>
+        <div className="flex items-center justify-center w-full">
+          <Radiooption
+            options={[
+              { label: "Sim", value: "option1" },
+              { label: "Não", value: "option2" },
+            ]}
+            selectedValue={selectedValue}
+            onChange={handleRadioChange}
+          />
+        </div>
+      </div>
+      <hr className="border-neutral border-4 my-8" />
+      <h3 className="font-bold uppercase mb-4">serviço de Fotografia</h3>
+
+      <div className="my-4 bg-[#463D8C1A] rounded-[10px] p-4 flex-col ">
+        <h3 className="font-semibold text-left text-base mb-2">
+          Semana 1 a 6 Julho
+        </h3>
+        <div className="flex items-center justify-center w-full">
+          <Radiooption
+            options={[
+              { label: "Sim", value: "option1" },
+              { label: "Não", value: "option2" },
+            ]}
+            selectedValue={selectedValue}
+            onChange={handleRadioChange}
+          />
+        </div>
+      </div>
+      <div className="my-4 bg-[#463D8C1A] rounded-[10px] p-4 flex-col ">
+        <h3 className="font-semibold text-left text-base mb-2">
+          Semana 1 a 6 Julho
+        </h3>
+        <div className="flex items-center justify-center w-full">
+          <Radiooption
+            options={[
+              { label: "Sim", value: "option1" },
+              { label: "Não", value: "option2" },
+            ]}
+            selectedValue={selectedValue}
+            onChange={handleRadioChange}
+          />
+        </div>
+      </div>
+      <hr className="border-primary border-4 my-8" />
+      <h2 className="text-lg mb-6">
+        <span className="font-semibold text-primary text-2xl mr-3">5.4</span>
+        E se tiver mais algum assunto ou observação a efetuar, não hesite. Escreva tudo o que acha relevante que nós estamos cá para o(a) ouvir.
+      </h2>
+      <textarea className="p-4 bg-neutral w-full rounded-[5px] placeholder:text-neutral-dark placeholder:font-semibold " placeholder="Mensagem" />
     </div>
   );
 };
